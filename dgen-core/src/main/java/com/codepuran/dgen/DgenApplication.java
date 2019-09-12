96cf353728871709a374f20627b002bb7fbf7e5e
Akshay Pethani <patelaksh412@gmail.com>
2019-09-12 23:07:53 +0530

    Initial Commit

---
+++ dgen-core/src/main/java/com/codepuran/dgen/DgenApplication.java
@@ -0,0 +1,13 @@
+package com.codepuran.dgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgenApplication.class, args);
+	}
+
+}
